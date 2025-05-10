c77768367e3901caf0de6a98aa113ba2d335cf72
RAJA <raja.mca@icloud.com>
2025-05-10 12:06:59 +0530

    async & await

--- scripts/advanced.js
+++ scripts/advanced.js
@@ -1,55 +1,26 @@
-function test() {
-    try {
-        let a = 10;
-        let b = 20;
-        return a + b;   // if any acident happens in the code, it will be caught in the catch block 
-    } catch (error) {
-        return "Error: Something went wrong";
+
+async function getData() {
+        // first call
+    const userData = await fetch("https://jsonplaceholder.typicode.com/users"); // 180ms
+    const userJson = await userData.json(); // 0
+        // second call
+    const photoData = await fetch("https://jsonplaceholder.typicode.com/photos");
+    const photoJson = await photoData.json();
+
+    return {
+        userData: userJson,
+        photoData: photoJson
     }
 }
-console.log("response of normal function calling", test());
 
-
-function test2 () {
-    return new Promise((resolve, reject) => {
-        let a = 10;
-        let b = 20;
-        if((a+b) > 100) {
-            reject("Error: Sum is greater than 100");
-        } else {
-            resolve("Success: Sum is less than 100");
-        }
-    });
-}
-
-test2()
-    .then((result) => {
-        console.log("response of promise function calling", result);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
-
-
-async function test3() {
-    //throw new Error("Error: Something went wrong"); // rejected value
-    return 10; // resolved value
-}
-
-test3()
-.then((result) => {
-    console.log("response of async function calling", result);
+getData().then(res=>{
+    console.log("two api response", res);
 }).catch((error) => {
     console.error(error);
-})
+});
 
 
 
-// async vs syncronous
-
-
-
-
-
-
-
+// async vs sync // parralel execution and one by one exceution
+// callback hell
+// async await

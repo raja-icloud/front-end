08352d574beba320b84a0b852a4fe62b0239fa13
RAJA <raja.mca@icloud.com>
2025-05-10 11:28:51 +0530

    async & promise & Exception completed

--- scripts/advanced.js
+++ scripts/advanced.js
@@ -1,15 +1,55 @@
-let a = 0;
-let b = 6;
-const result = b / a; // file read or write
-if(result === Infinity) throw new Error('test')
-try {
-	const result = b / a; // file read or write
-	if(result === Infinity) throw new Error('test')
-} catch(e) {
-	console.error("error happened", e.message)
-} finally {
-	console.log("execute finally")
+function test() {
+    try {
+        let a = 10;
+        let b = 20;
+        return a + b;   // if any acident happens in the code, it will be caught in the catch block 
+    } catch (error) {
+        return "Error: Something went wrong";
+    }
+}
+console.log("response of normal function calling", test());
+
+
+function test2 () {
+    return new Promise((resolve, reject) => {
+        let a = 10;
+        let b = 20;
+        if((a+b) > 100) {
+            reject("Error: Sum is greater than 100");
+        } else {
+            resolve("Success: Sum is less than 100");
+        }
+    });
 }
 
-console.log('hello');
-// =======================================
+test2()
+    .then((result) => {
+        console.log("response of promise function calling", result);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+
+async function test3() {
+    //throw new Error("Error: Something went wrong"); // rejected value
+    return 10; // resolved value
+}
+
+test3()
+.then((result) => {
+    console.log("response of async function calling", result);
+}).catch((error) => {
+    console.error(error);
+})
+
+
+
+// async vs syncronous
+
+
+
+
+
+
+
